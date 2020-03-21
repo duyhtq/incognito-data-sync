2a17aae1dde8e898194478c8b7c412f3ada2dd41
hoangphuongcs <phuong@autonomous.nyc>
2020-03-21 16:55:21 +0700

    uncomment to tét

--- extensions/cron_sync_data/main.go
+++ extensions/cron_sync_data/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"runtime"
 	"strconv"
 	"syscall"
 	"time"
@@ -222,14 +223,14 @@ func executeAgent(
 }
 
 func main() {
-	// runtime.GOMAXPROCS(runtime.NumCPU())
-	// s, err := NewServer()
-	// if err != nil {
-	// 	return
-	// }
-	// s.Run()
-	// for range s.agents {
-	// 	<-s.finish
-	// }
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	s, err := NewServer()
+	if err != nil {
+		return
+	}
+	s.Run()
+	for range s.agents {
+		<-s.finish
+	}
 	fmt.Println("Server stopped gracefully!")
 }
